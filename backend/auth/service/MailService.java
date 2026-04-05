27bdb5c06c492b6e37501b2180a9a8d75b87acba
Loint99 <lointts02286@gmail.com>
2026-04-05 16:33:08 +0700

    Delete backend/auth directory

--- backend/auth/service/MailService.java
+++
@@ -1,7 +0,0 @@
-package com.company.hrm.module.auth.service;
-
-public interface MailService {
-    void sendPasswordResetMail(String email, String username, String resetLink);
-
-    boolean sendSimpleMail(String email, String subject, String body);
-}
