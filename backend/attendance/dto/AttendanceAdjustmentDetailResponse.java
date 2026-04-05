13cfe013b32c1b52e90fef8a50fd5e3904eea7f9
Loint99 <lointts02286@gmail.com>
2026-04-05 16:32:48 +0700

    Delete backend/attendance directory

--- backend/attendance/dto/AttendanceAdjustmentDetailResponse.java
+++
@@ -1,9 +0,0 @@
-package com.company.hrm.module.attendance.dto;
-
-import java.util.List;
-
-public record AttendanceAdjustmentDetailResponse(
-        AttendanceAdjustmentListItemResponse request,
-        List<AttendanceAdjustmentHistoryResponse> history
-) {
-}
